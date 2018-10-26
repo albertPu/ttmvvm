db088bbb962614c4779e1624e058c0da18edecf7
pxy <526966586@qq.com>
2018-10-26 23:51:42 +0800

    无法访问

--- app/src/main/java/tt/cc/com/ttmvvm/MainActivity.kt
+++ app/src/main/java/tt/cc/com/ttmvvm/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setStatusColor(true)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/tt/cc/com/ttmvvm/net/ApiStore.java
+++ app/src/main/java/tt/cc/com/ttmvvm/net/ApiStore.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class ApiStore {
     private static Retrofit retrofit;
     private static OkHttpClient okHttpClient;
-    private static final String BASE_URL = "http://172.16.30.27:8080/";
+    private static final String BASE_URL = "http://192.168.0.9:8080/";
     private static final long DEFAULT_TIME_OUT = 20;
     private static final long DEFAULT_READ_TIME_OUT = 20;
 
